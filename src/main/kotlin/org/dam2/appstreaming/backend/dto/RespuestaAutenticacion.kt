1fce06b5bbcba605e0a2d2c7fce021dbe3a026aa
albacmerino <sergio.gargoles@educa.madrid.org>
2026-04-14 14:04:12 +0200

    Eliminado el backend con PostgreSQL

--- src/main/kotlin/org/dam2/appstreaming/backend/dto/RespuestaAutenticacion.kt
+++
@@ -1,9 +0,0 @@
-package org.dam2.appstreaming.backend.dto
-
-/**
- * Respuesta enviada tras un login o registro exitoso.
- */
-data class RespuestaAutenticacion(
-    val token: String,
-    val nombreUsuario: String
-)
